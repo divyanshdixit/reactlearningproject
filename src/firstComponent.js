4d7c0b165b4712a50700378a8f48bd0f61b743a8
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-01-16 19:15:06 +0530

    installation and create first component file

---
+++ src/firstComponent.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function firstComponent(){
+    return (
+        <div className="first_component">
+            <h1> This is our first custom component. </h1>
+            <p> Something about out first component.</p>
+        </div>
+    );
+}
+
+export default firstComponent;
