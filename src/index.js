ce56b88403d6952e693166b5ebe3aab41f9e50a8
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-01-18 23:25:39 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'; // use html element
+// import React from 'react'; // use html element
 import ReactDOM from 'react-dom'; // dom manupulation
 import './index.css';
 import App from './App';
